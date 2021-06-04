111057767fe030f2b40f09eb516df8b80587df41
Zoryana Herman <z.herman@ucu.edu.ua>
2021-06-04 16:29:39 +0300

    Fixed Contacts

--- src/modules/Contacts/components/GenerealContacts/index.jsx
+++ src/modules/Contacts/components/GenerealContacts/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import LeftContacts from "../LeftContacts";
-import RightContacts from "../RightContacts";
+import Contacts from "../Contacts";
 
 const GeneralContacts = () => {
 
     return (
         <div className="row" id="i6g53">
 
-            <LeftContacts/>
+            <Contacts name="Northern Hill" email="patsserie.northern.hill@ucu.edu.ua" phone="0 (12) 345 678 90" beginTime="10 am" endTime="6 pm"/>
 
-            <RightContacts/>
+            <Contacts name="Southern Hill" email="patsserie.southern.hill@ucu.edu.ua" phone="0 (12) 457 381 49" beginTime="9 am" endTime="5 pm"/>
+
 
         </div>
     );
